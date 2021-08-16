d228e9a9753d72264d6d3ddbd518e4a5e5275d96
sid773 <89004995+sid773@users.noreply.github.com>
2021-08-16 12:46:54 +0530

    Add files via upload
    simple signup js file

---
+++ 0variables.js
@@ -0,0 +1,27 @@
+const uid ="abc123";
+
+var firstName ="sid";
+var country ="india";
+var relegon="hindu";
+var email="sid@.com";
+var password ="123456";
+var confirmPassword ="123456";
+var courseCount =0;
+var isLoggedInFromGoogle= false;
+
+//fullname=prompt("enter ur name");
+console.log(uid);
+console.log("ull name is:",firstName);
+console.log("his country",country);
+console.log("relegon",relegon)
+console.log(email);
+console.log(`
+      with Unique ID:${uid}
+      user is :${firstName}
+      and his counry:${country}
+      and his relegon:${relegon}
+      and his email is :${email}
+      and uses the password : ${password}
+
+      `);
+
